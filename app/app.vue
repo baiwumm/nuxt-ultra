ac5379320efbb922aa9a1c9bf1316a938f5eb07d
baiwumm <me@baiwumm.com>
2025-12-06 00:18:42 +0800

    feat: 初始化项目

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { x, y } = useMouse()
+</script>
+
+<template>
+  <div class="flex flex-col h-screen s-screen items-center justify-center gap-4">
+    <div class="text-lg font-bold">
+      Nuxt.js
+    </div>
+    <div>pos: {{ x }}, {{ y }}</div>
+    <Button>Click me</Button>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,46 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.baiwumm.com/fonts/MapleMono-CN-Regular/result.css',
+        },
+      ],
+    },
+  },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['shadcn-nuxt', '@vueuse/nuxt', '@nuxtjs/color-mode'],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: './app/components/ui',
+  },
+
+  colorMode: {
+    classSuffix: '',
+  },
+})
